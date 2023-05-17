40cfdd7e55e44c4d0d7680586cc157eb2bb78d8b
Zagrivaya <zagrivaya@yandex.ru>
2023-05-17 14:57:19 +0300

    add calc cropped average

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
         int[] sales = new int[] {100, 200, 300, 400, 500, 600};
         SalesManager salesManager = new SalesManager(sales);
         int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
+        System.out.println(min);
         System.out.println(max);
+        System.out.println(average);
     }
 }
