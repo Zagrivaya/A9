7bd12c7909e82b740c756afdf7b2d72262d98450
Zagrivaya <zagrivaya@yandex.ru>
2023-05-17 13:21:46 +0300

    switch to longer, int type on long

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = new int[] {100, 200, 300, 400, 500, 600};
+        long[] sales = new long[] {100, 200, 300, 400, 500, 600};
         SalesManager salesManager = new SalesManager(sales);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
     }
 }
